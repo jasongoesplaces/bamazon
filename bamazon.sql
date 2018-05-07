be32b44604ea9d1d17599a53d57c10685764dab3
Jason <jasongoesplaces@gmail.com>
2018-05-07 00:55:24 -0400

    screenshots added and database changes

--- bamazon.sql
+++ bamazon.sql
@@ -13,13 +13,11 @@ Select * from products;
 
 INSERT INTO products (product, department, price, stock)
 VALUES ("iPhone 8", "Electronics", 800.00, 50),
-("Hockey Puck", "Sports", 10.98, 10),
-("Pack of Sponges", "Kitchen", 15.00, 15),
 ("Digital Camera", "Electronics", 1000.00, 30),
+("Flat Screen TV", "Electronics", 2000.00, 50),
+("Hockey Puck", "Sports", 10.98, 10),
 ("Baseball Bat", "Sports", 45.00, 5),
 ("Soccer Ball", "Sports", 25.00, 70),
-("Tent", "Outdoors", 190.00, 10),
 ("Camping Stove", "Outdoors", 300.00, 10),
-("Flat Screen TV", "Electronics", 2000.00, 50),
-("Dishes Set", "Kitchen", 34.99, 46),
+("Tent", "Outdoors", 190.00, 10),
 ("Sleeping Bag", "Outdoors", 70.00, 65);
